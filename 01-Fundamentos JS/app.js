eabff88871ea5c2dcf336c9c5ce59c54ce549811
lucascn21 <lucascn21@gmail.com>
2020-06-03 20:38:28 -0300

    vimos variables

--- 01-Fundamentos JS/app.js
+++ 01-Fundamentos JS/app.js
@@ -1,1 +1,8 @@
 console.log("Js desde archivo");
+var primerNombre = 'Gianzo';
+console.dir(primerNombre);
+
+//variable declarada
+var variableDeclarada;
+//inicializo la variable
+variableDeclarada = "queso";
